95710ecc13f530d7b695071f2cb1347f16e479fb
Pashan Irani <pashan@Pashans-MBP-2.hitronhub.home>
2017-09-10 09:15:55 -0700

    generated a random image

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello World") #this is all of python I know :P
+import png
+import random
+
+file_name = 'coloredImage'
+test_png = []
+png_size = 512
+color = [random.randint(0,255),
+        random.randint(0,255),
+        random.randint(0,255)]
+
+for_loop_iterations = 0
+for i in range(0, png_size):
+    test_png.append([]) #creating rows
+
+    #add RGB pixels for row (will be based on png_size)
+    for e in range(0, png_size):
+        # adds each RBG value to the row
+        for y in range (0, len(color)):
+            test_png[i].append(color[y])
+            ##for_loop_iterations++ lol, python doesn't support ++
+            for_loop_iterations += 1
+
+
+png.from_array(test_png, 'RGB').save(file_name + '.png')
+print('\n')
+print('\\\\\\\\\\\\\\\\')
+print('| _   _|')
+print('|   |  |')
+print('|  _|  |    -- when I wasted my sunday morning doing this shit...')
+print('|      |')
+print('|     |')
+print('|     |')
+print('\n')
+print('%s.png of size %dx%d was create after %d iterations, lol' % (file_name, png_size, png_size, for_loop_iterations))
+print('\n')
